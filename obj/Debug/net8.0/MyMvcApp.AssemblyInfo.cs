5b6e974db291e4e8d483c88d27e0c3f4ca4c4383
mihiryadav20 <mihir.yadav@somaiya.edu>
2025-08-04 16:30:21 +0530

    status tweaks

--- obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9535b8796fb305ac61a51146a484055f8dda187e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d5349db2801717905b6cd26056a5de4b051efb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
